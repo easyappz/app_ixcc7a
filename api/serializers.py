931a762a0275d705113f44177ba774e69d104c80
IbragimKhaniev <newinterny@gmail.com>
2025-11-24 01:01:15 +0300

    Add Member, Register, Login, Message and MessageCreate serializers

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,46 @@
 from rest_framework import serializers
+from api.models import Member, Message
 
 
-class MessageSerializer(serializers.Serializer):
-    message = serializers.CharField(max_length=200)
-    timestamp = serializers.DateTimeField(read_only=True)
+class MemberSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Member
+        fields = ['id', 'username', 'is_online', 'created_at']
+        read_only_fields = ['id', 'created_at', 'is_online']
+
+
+class RegisterSerializer(serializers.Serializer):
+    username = serializers.CharField(max_length=150)
+    password = serializers.CharField(write_only=True, min_length=4)
+
+    def create(self, validated_data):
+        member = Member(
+            username=validated_data['username']
+        )
+        member.set_password(validated_data['password'])
+        member.save()
+        return member
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+
+class MessageSerializer(serializers.ModelSerializer):
+    sender_id = serializers.IntegerField(source='sender.id', read_only=True)
+    sender_username = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Message
+        fields = ['id', 'sender_id', 'sender_username', 'text', 'timestamp']
+        read_only_fields = ['id', 'sender_id', 'sender_username', 'timestamp']
+
+    def get_sender_username(self, obj):
+        return obj.sender.username
+
+
+class MessageCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Message
+        fields = ['text']
